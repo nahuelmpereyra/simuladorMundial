7d8d1bf021552000806bdf6b1a73b45773a1cf6c
nahuelmpereyra <nahuelmpereyra@gmail.com>
2018-06-09 01:30:05 -0300

    Agregados metodos get equipo y post para crear equipo, faltan validaciones

--- src/main/java/Services.java
+++ src/main/java/Services.java
@@ -1,33 +1,52 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import gherkin.deps.com.google.gson.JsonObject;
 
-// The Java class will be hosted at the URI path "/helloworld"
-@Path("/index")
-public class Services {
+import javax.persistence.PostRemove;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+
+@Path("")
+public class Services{
     // The Java method will process HTTP GET requests
     TestService testService = new TestService();
-//    @GET
-//    // The Java method will produce content identified by the MIME Media type "text/plain"
-//    @Produces("text/plain")
-//    public String getClichedMessage() {
-//        // Return some cliched textual content
-//       Equipo equipo1 = this.testService.recuperarEntidad(Equipo.class, "Argentina");
-//      // Equipo equipo2 = this.testService.recuperarEntidad(Equipo.class, "Islandia");
-//       Equipo equipo3 = this.testService.recuperarEntidad(Equipo.class, "Croacia");
-//       return equipo1.getNombre() + equipo3.getNombre();
-//
-//    }
+
 
     @GET
+    @Path("/index")
     @Produces("application/json")
-    public String getEquipos() {
+    public Equipo getEquipos() {
         // Return some cliched textual content
         Equipo equipo1 = this.testService.recuperarEntidad(Equipo.class, "Argentina");
         Equipo equipo2 = this.testService.recuperarEntidad(Equipo.class, "Islandia");
 //        Equipo equipo3 = this.testService.recuperarEntidad(Equipo.class, "Croacia");
-        return equipo1.getNombre() +"\n"+ equipo2.getNombre();
+        return equipo1;
 
     }
 
+    @GET
+    @Path("{pais}")
+    @Produces("application/json")
+    public Equipo getEquipobyId(@PathParam("pais") String pais) {
+        Equipo equipo1 = this.testService.recuperarEntidad(Equipo.class, pais);
+        return equipo1;
+    }
+
+    @POST
+    @Path("/agregarequipo")
+    @Consumes({"application/json"})
+    @Produces({"text/plain"})
+    public Response createEquipo(Equipo equipo){
+
+        this.testService.crearEntidad(equipo);
+
+        return Response.ok(this.testService.recuperarEntidad(Equipo.class, equipo.getNombre()).getNombre() + " agregado con éxito").build();
+//        return Response.ok(this.testService.recuperarEntidad(Equipo.class, equipo.getNombre())).build();
+    }
+
+
+
 }
+// The Java class will be hosted at the URI path "/index"
+
+
+
